e35d5f9b2f8af5d7ac8d7311641defc90d5e94e0
esarmientoe <eduardo.sarmiento@tss.com.pe>
2016-11-04 17:06:10 -0500

    Quick fixes to get it to run on Windows

--- knexfile.js
+++ knexfile.js
@@ -3,15 +3,20 @@ require('dotenv').load();
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgresql://localhost:5432/dev'
+    connection: process.env.DATABASE_URL || 'postgresql://localhost:5432/',
+	connection: {
+      user: process.env.USER || 'postgres',
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME
+    }
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL || 'postgresql://localhost:5432/dev',
+    connection: process.env.DATABASE_URL || 'postgresql://localhost:5432/',
     connection: {
-      user: 'root',
+      user: process.env.USER || 'postgres',
       password: process.env.DATABASE_PASSWORD,
-      database: 'dev'
+      database: process.env.DATABASE_NAME
     }
   }
 };
